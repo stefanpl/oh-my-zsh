411fbea5fd760c2a13f9e50f1bbd5dc549b9f998
spl <stefanpl@gmx.net>
2014-11-12 13:03:23 +0100

    customizing zsh on 'stefan' branch

---
+++ .bash_functions
@@ -0,0 +1,5 @@
+
+# creates a directory and cd-s into it right after
+makeDirectoryAndGoThere() {
+	mkdir $1 && cd $_
+}
--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -33,3 +33,30 @@ alias la='ls -lAh'
 
 alias afind='ack-grep -il'
 
+#########################################
+# Stefan's aliases
+#########################################
+
+alias wowscreen="sh /home/stefan/scripts/wowscreen.sh"
+alias kp="keepass2 &"
+alias mkd="makeDirectoryAndGoThere"
+alias h="cd ~/"
+alias s="git status"
+alias rma="sudo rm -Rfv"
+alias keys="sudo subl /usr/share/X11/xkb/symbols/de"
+alias tt="sh /home/stefan/scripts/toggleTouchpad.sh"
+alias ip="ifconfig | egrep \"inet addr:192.168.[0-9]+.[0-9]+\" -o"
+alias down="sudo shutdown -h now 'all the way down...'"
+alias rebo="sudo shutdown -r now \"I'll be back!\""
+alias off="sleep 1s; sh /home/stefan/scripts/toggleScreen.sh off"
+alias vimrc="vim ~/.vimrc"
+alias ..="cd .."
+alias gl="git log --oneline -10 | cat"
+alias ll="ls -aFl --block-size=K"
+alias aliases="vim /home/stefan/.oh-my-zsh/lib/aliases.zsh; source ~/.oh-my-zsh/lib/aliases.zsh"
+alias aliases_private="vim /home/stefan/.bash_aliases_private ;source /home/stefan/.bash_aliases_private"
+alias -g le="| less"
+alias -g gr="| grep -i"
+alias zshrc="vim ~/.zshrc; source ~/.zshrc"
+
+
