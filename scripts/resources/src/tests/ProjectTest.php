e436c00a2cc741f32e17c516e02cedb4836e4a3d
spl <stefanpl@gmx.net>
2016-12-18 19:15:24 +0100

    create project script

---
+++ scripts/resources/src/tests/ProjectTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: stefan
+ * Date: 18.12.16
+ * Time: 14:44
+ */
+class ProjectTest extends PHPUnit_Framework_TestCase
+{
+
+
+    /**
+     * Valid project names are made up of [_-a-z]
+     */
+    public function testProjectNames()
+    {
+
+    }
+
+}
